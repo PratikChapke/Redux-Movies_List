c5284205c45f15d7f43ddab9872bea5dcc0c3462
pratik <pratik.chapke@gmail.com>
2023-02-15 00:33:36 +0530

    add redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'react-redux';
+
 import './index.css';
 import App from './components/App';
 
+const store = createStore ;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
